83456d0df6193bd7144142130a4205afa3af17d1
Vo Phuoc Thinh <vphuocthinh1@gmail.com>
2025-09-13 10:50:28 +0700

    Add files via upload

---
+++ sapxeptimkiem/28tech30.cpp
@@ -0,0 +1,109 @@
+/** TO MY NOTION:
+
+                                               ,----'   `-,
+
+                     ___,-'~~~`---'~~~~`-----' ,-'        \,
+
+             ___,---'          '        ,-~~~~~            :
+
+        ,--'                 ,        ; ;       ) "   _   \,
+
+   _,--'     ,                 ,'      :: "  ;  ` ,'  (\o\)  |
+
+  / _,       `,                     ,  `; " ;    (     `~~ `'\
+
+ ; /         ,`               ,     `    ~~~`. " ;   _     ,  `.
+
+,'/          ` ,              `     ` ,  ,    \_/ ?   ;    )   `.
+
+:;:            `                      `  ` ,     uu`--(___(    ~:
+
+:::          , ,  ,            ,   ;     , `  ,-'      \~(  ~   :
+
+||:          ` `  `         ,  ` ,'    ( ` ,-          \ \   ~:
+
+:|`.        , ,  ,          `_   ;       ) );            \__>   :
+
+|:  : ;     ` `  ` ;  __,--~~ `-(         ( |              `.  ~|
+
+:|  :         ` __,--'           :  ()    : :               |~  ;
+
+|;  |  `     ,-'    ;             :  )(   | `.         /(   `. ~|
+
+::  :   ~  _/;     ;               |   )  :  :        ; /    ;~ ;
+
+{}  ;     /  :   ~ :               :      ; ,;        : `._,-  ,
+
+{} /~    /   ;    ;                 : ,  |  `;         `.___,-'
+
+  ;~    ;    ;  ~ `.                | `  )   ;
+
+  :`    \    `;~   \                ;~   `-, `-.     VOI24
+
+  `.__OOO;    `;_OOO;               )_____OO;_(O)
+
+   ~~~~~~       ~~~~                ~~~~~~~~ ~~~~
+
+   */
+
+#include<bits/stdc++.h>
+////Ta thuat
+//#pragma GCC optimize("O2")
+//#pragma GCC target("avx,avx2,fma")
+#define MOD 1000000007
+#define fi first
+#define se second
+#define int long long
+#define ii pair<int,int>
+#define Dennis "Top1Server"
+#define Jack1e "DB bainao"
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+void file()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+}
+int n, k, a[10001];
+bool check(int len){
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		ans+=1ll*len/a[i];
+	}
+	return ans >=k;
+}
+int main(){
+	cin >> n >> k;
+	for(int &x : a) cin >> a[i];
+	int l = 0, r = 1e18, res = 0;
+	rep(i, 120){
+		double mid = l+r>>1;
+		if(check(mid)){
+			l = m;
+		}else r = m;
+	}
+	cout << fixed << setprecision(6) << l << endl;
+}
---
+++ stack/28tech9.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define pii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define inf 9223372036854775807
+#define nl endl
+#define prDouble(x,y) cout << fixed << setprecision(y) << x
+#define pll pair<ll, ll>
+#define spi set<pii>
+#define si set<int>
+#define mpci map<char, ll>
+#define mpii map<ll, ll>
+#define f(i,a,n) for(ll i=a;i<n;i++)
+#define fr(i,n,a) for(ll i=n-1;i>=0;i--)
+#define M 1000000007
+#define ci cin
+#define co cout
+#define maxn 1000005
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+
+/*------------------------------------SOME BASIC FUNCTION--------------------------------------------------*/
+
+ll sumofdigit(ll n){ll s = 0;while (n > 0){ll r = n % 10;s += r;n = n / 10;}return s;}
+ll gcd(ll a, ll b){if (b % a == 0){return a;}else{return gcd(b % a, a);}}
+ll POW(ll x, ll y){ll ans=1;for(ll i=0;i<y;i++){ans=ans*x;}return ans;}
+ll ndigit(ll n){ll s=0;while(n>0){s++;n=n/10;}return s;}
+ll arraysum(ll a[], ll n){ll s = 0;f(i, 0, n){s = s + a[i];}return s;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;}//O(sqrt(N))
+bool isPrime(int n){if (n <= 1) return false; for (int i = 2; i < n; i++)if (n % i == 0) return false; return true;}
+int binExpo(int a, int b){ if(b==0) return 1;if(b==1) return a; int r = binExpo(a, b/2); if(b%2==0) return (r%M * r%M)%M; else return (r%M*r%M*a%M)%M;}
+int fast_mul(int x, int y){if (x == 0) return 0; else if (x % 2 == 1) return (fast_mul(x >> 1, y << 1) + y); else return fast_mul(x >> 1, y << 1);}
+
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+
+/*-----------------------------------ACTUAL CODE STARTS HERE-----------------------------------------------------*/
+
+void file()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+}
+int main(){
+	string s; cin >> s;
+	reverse(all(s));
+	stack<string> st;
+	string res = "";
+	for(char x : s){
+		if(isalpha(x)){
+			st.push(string(1, x));
+		}else{
+			string tmp1 = st.top(); st.pop();
+			string tmp2 = st.top(); st.pop();
+			st.push(tmp1 + tmp2 + x);
+		}
+	}
+	cout << st.top();
+}
+
+//+*xyz
+//zyx*+
+//zyx
+// x dung dau, y dung 2, z dung 3 vay x, y, => x*y la st.top() moi va sang xy*z+
