648df07a71f30f77e365ddc11b78c05bbb0f5e55
Vo Phuoc Thinh <vphuocthinh1@gmail.com>
2025-09-13 10:48:25 +0700

    Add files via upload

---
+++ queue/28tech8.cpp
@@ -0,0 +1,119 @@
+/** TO MY NOTION:
+
+                                               ,----'   `-,
+
+                     ___,-'~~~`---'~~~~`-----' ,-'        \,
+
+             ___,---'          '        ,-~~~~~            :
+
+        ,--'                 ,        ; ;       ) "   _   \,
+
+   _,--'     ,                 ,'      :: "  ;  ` ,'  (\o\)  |
+
+  / _,       `,                     ,  `; " ;    (     `~~ `'\
+
+ ; /         ,`               ,     `    ~~~`. " ;   _     ,  `.
+
+,'/          ` ,              `     ` ,  ,    \_/ ?   ;    )   `.
+
+:;:            `                      `  ` ,     uu`--(___(    ~:
+
+:::          , ,  ,            ,   ;     , `  ,-'      \~(  ~   :
+
+||:          ` `  `         ,  ` ,'    ( ` ,-          \ \   ~:
+
+:|`.        , ,  ,          `_   ;       ) );            \__>   :
+
+|:  : ;     ` `  ` ;  __,--~~ `-(         ( |              `.  ~|
+
+:|  :         ` __,--'           :  ()    : :               |~  ;
+
+|;  |  `     ,-'    ;             :  )(   | `.         /(   `. ~|
+
+::  :   ~  _/;     ;               |   )  :  :        ; /    ;~ ;
+
+{}  ;     /  :   ~ :               :      ; ,;        : `._,-  ,
+
+{} /~    /   ;    ;                 : ,  |  `;         `.___,-'
+
+  ;~    ;    ;  ~ `.                | `  )   ;
+
+  :`    \    `;~   \                ;~   `-, `-.     VOI24
+
+  `.__OOO;    `;_OOO;               )_____OO;_(O)
+
+   ~~~~~~       ~~~~                ~~~~~~~~ ~~~~
+
+   */
+
+#include<bits/stdc++.h>
+////Ta thuat
+//#pragma GCC optimize("O2")
+//#pragma GCC target("avx,avx2,fma")
+#define MOD 1000000007
+#define fi first
+#define se second
+#define int long long
+#define ii pair<int,int>
+#define Dennis "Top1Server"
+#define Jack1e "DB bainao"
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+void file()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+} 
+main(){
+  file();
+  int t; cin >> t;
+  while(t--){
+  	int k; cin >> k;
+  	string s; cin >> s;
+  	map<char,int> mp;
+  	for(char x : s){
+  		mp[x]++;
+  	}
+  	priority_queue<int, vector<int>> q;
+  	for(auto x : mp){
+  		q.push(x.second);
+  	}
+  	while(!q.empty() && k > 0){
+  		--k;
+  		int top = q.second; q.pop();
+  		top--;
+  		if(top > 0) q.push(top);
+  	}
+  	int ans = 0;
+  	while(!q.empty()){
+  		int res = 1ll * q.top() * q.top();
+  		ans+=res;
+  		q.pop();
+  	}
+  	cout << ans << endl;
+  }
+  return 0;
+}
